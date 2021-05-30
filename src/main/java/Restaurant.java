6e2b0a591b2ca52b43dbb32b18aa124bbf7856fd
siddharth <siddharth.ashutosh7@gmail.com>
2021-05-30 22:31:55 +0530

    Part 2: Solution

--- src/main/java/Restaurant.java
+++ src/main/java/Restaurant.java
@@ -18,15 +18,17 @@ public class Restaurant {
     }
 
     public boolean isRestaurantOpen() {
-        return true;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+        if((this.getCurrentTime().isBefore(this.closingTime) && this.getCurrentTime().isAfter(this.openingTime)) || this.getCurrentTime().equals(this.openingTime)){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     public LocalTime getCurrentTime(){ return  LocalTime.now(); }
 
     public List<Item> getMenu() {
-        return null;
-        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return this.menu;
     }
 
     private Item findItemByName(String itemName){
--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -5,9 +5,22 @@ import java.util.List;
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
-        return null;
-        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+    public Restaurant findRestaurantByName(String restaurantName) throws restaurantNotFoundException
+    {
+        boolean isRestaurantFound = false;
+        Restaurant restaurantFoundName = null;
+        for(Restaurant restaurant: restaurants){
+            if(restaurant.getName().equals(restaurantName)){
+                isRestaurantFound = true;
+                restaurantFoundName =  restaurant;
+                break;
+            }
+        }
+        if(isRestaurantFound){
+            return  restaurantFoundName;
+        }else {
+            throw new restaurantNotFoundException(restaurantName);
+        }
     }
 
 
